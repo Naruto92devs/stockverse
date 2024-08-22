48d76ae1e8ee7c5d9387cdbacaad9d90ad73c01c
Naruto92devs <m.suhaibmpdgw@gmail.com>
2024-08-22 18:38:54 +0500

    adding fallback for sun icon

--- src/components/ThemeSwitch.js
+++ src/components/ThemeSwitch.js
@@ -17,7 +17,7 @@ const handleClick = () => {
 useEffect(() => setMounted(true), [])
 
 if (!mounted) return (
-<Image className=" w-[1px] h-[1px]"
+<Image className=" w-[42px] h-[42px]"
     src="data:image/svg+xml;base64,PHN2ZyBzdHJva2U9IiNGRkZGRkYiIGZpbGw9IiNGRkZGRkYiIHN0cm9rZS13aWR0aD0iMCIgdmlld0JveD0iMCAwIDI0IDI0IiBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMjAwcHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjIwIiBoZWlnaHQ9IjIwIiB4PSIyIiB5PSIyIiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjIiIHJ4PSIyIj48L3JlY3Q+PC9zdmc+Cg=="
     width={42}
     height={42}
