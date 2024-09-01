ec6128cbd99bacc677df300ff906844ffc0dc3d5
Muhammad Suhaib <muhammadsuhaib@192.168.1.19>
2024-09-01 20:07:39 +0500

    adding some fixes for container issues

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="py-8 w-full bg-heroBg bg-no-repeat bg-cover bg-left-bottom">
       <div className="px-6 mx-auto xl:container flex flex-col items-center">
-        <h1 className="text-9xl">Invest The Smart Way - Caiman Invest</h1>
+        <h1 className="text-xl">Invest The Smart Way - Caiman Invest</h1>
       </div>
     </div>
   );
