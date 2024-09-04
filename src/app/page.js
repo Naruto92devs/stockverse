bb95d08b32f01df943b134cf3cb9bcbf615950cd
Naruto92 <m.suhaibmpdgw@gmail.com>
2024-09-04 22:59:05 +0500

    remoed extra stokcs

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import StocksList from '../components/StocksList.js'
-import Yourstocks from '@/components/yourstocks.js';
+// import Yourstocks from '@/components/yourstocks.js';
 
 export default function Home() {
   return (
@@ -8,7 +8,7 @@ export default function Home() {
         <h1 className="w-[70%] font-sansBold max-sm:w-[100%] text-[70px] max-xl:text-[5.2vw] max-sm:text-[7.5vw] leading-[120%] font-bold text-primaryHeading text-center">Your Gateway to Smarter Decision<span className="text-secondaryHeading">  - Stockverse</span></h1>
         <p className="text-2xl w-[50%] max-xl:w-[70%] max-sm:w-[90%] leading-[120%] max-xl:text-[2vw] max-sm:text-[4vw] text-center text-secondaryHeading">Discover real-time stock data, personalized insights, and AI-driven recommendations tailored to your trading style.</p>
         <StocksList/>
-        <Yourstocks/>
+        {/* <Yourstocks/> */}
       </div>
     </div>
   );
