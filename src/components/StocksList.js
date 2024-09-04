ccb3c769dc694a86bec3d176630dc7cba354948f
Naruto92 <m.suhaibmpdgw@gmail.com>
2024-09-04 22:47:38 +0500

    adding api stockverse

--- src/components/StocksList.js
+++ src/components/StocksList.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 async function StocksList() {
 
 // Fetch data from the new API on the server side
-const res = await fetch('https://devsalman.tech/top7');
+const res = await fetch('https://api.stockverse.ai/top7');
 const data = await res.json();
 
 // Utility function to format large numbers (e.g., 1000000 -> 1M, 250000 -> 250K)
