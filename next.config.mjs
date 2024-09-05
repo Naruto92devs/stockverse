a765ff61884a1759734b7a3d0a57099c6200cb24
Naruto92 <m.suhaibmpdgw@gmail.com>
2024-09-06 03:00:57 +0500

    adding fix to api not resposponding again

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['img.logo.dev'],  // Add 'logo.clearbit.com' to the list of allowed domains
+        remotePatterns: [
+            {
+              protocol: 'https', // Protocol used by the domain (http or https)
+              hostname: 'img.logo.dev', // Replace with your domain
+              port: '', // Leave empty if no specific port is needed
+              pathname: '/**', // Matches any path after the domain
+            },
+        ]  // Add 'logo.clearbit.com' to the list of allowed domains
     },
 };
 
