c72f4a64e820d490fee1845f8c859ef07bd3cac6
Naruto92 <m.suhaibmpdgw@gmail.com>
2024-09-15 03:23:39 +0500

    add

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,15 +5,29 @@ import Navbar from '@/components/Navbar';
 import { ThemeProvider } from '../context/ThemeContext';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import Footer from '@/components/Footer';
+import Head from 'next/head';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
+      <Head>
         <meta name="description" content="Discover real-time stock data, expert financial analysis, and market insights on Stockverse. Stay ahead of the market with live updates, in-depth stock news, IPO calendars, and personalized tools to help you make informed investment decisions." />
         <link rel="icon" href="/favicon.png" size="92*92" />
         <title>Stockverse</title>
-      </head>
+        {/* Google Tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-PEDC750L6H"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-PEDC750L6H');
+              gtag('config', 'AW-16488126373');
+            `,
+          }}
+        />
+      </Head>
       <body className="bg-background w-[100%] mx-auto">
         <Providers>
           <NextThemesProvider>
