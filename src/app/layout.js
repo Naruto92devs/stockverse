5b52ed1c3d6f56b849b00a3ba725060a80dd4614
Naruto92 <m.suhaibmpdgw@gmail.com>
2024-09-15 03:33:32 +0500

    adding g tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,29 +5,29 @@ import Navbar from '@/components/Navbar';
 import { ThemeProvider } from '../context/ThemeContext';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import Footer from '@/components/Footer';
-import Head from 'next/head';
+// import script from 'next/script';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <Head>
+      <head>
         <meta name="description" content="Discover real-time stock data, expert financial analysis, and market insights on Stockverse. Stay ahead of the market with live updates, in-depth stock news, IPO calendars, and personalized tools to help you make informed investment decisions." />
         <link rel="icon" href="/favicon.png" size="92*92" />
         <title>Stockverse</title>
-        {/* Google Tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-PEDC750L6H"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-PEDC750L6H');
-              gtag('config', 'AW-16488126373');
-            `,
-          }}
-        />
-      </Head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=G-PEDC750L6H`}></script>
+        <script>
+          {
+            `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+  
+            gtag('config', 'G-PEDC750L6H');
+            `
+          }
+        </script>
+      </head>
       <body className="bg-background w-[100%] mx-auto">
         <Providers>
           <NextThemesProvider>
