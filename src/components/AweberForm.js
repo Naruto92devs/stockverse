e8fb9a81b81ac09fb9db70f79385879aac794d41
Naruto92 <m.suhaibmpdgw@gmail.com>
2024-09-15 04:37:00 +0500

    adding changes to form

--- src/components/AweberForm.js
+++ src/components/AweberForm.js
@@ -1,29 +1,41 @@
 'use client';
-// components/AweberForm.js
+
 import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 const AweberForm = () => {
+  const router = useRouter();
+
   useEffect(() => {
-    // Check if the script already exists to avoid adding it multiple times
     const scriptId = 'aweber-wjs-utlz97g6k';
-    if (document.getElementById(scriptId)) return;
 
-    // Create a script element
-    const script = document.createElement('script');
-    script.id = scriptId;
-    script.src = '//forms.aweber.com/form/47/954193347.js';
-    script.async = true;
+    // Function to add the script
+    const addScript = () => {
+      // Remove existing script if any
+      if (document.getElementById(scriptId)) {
+        document.getElementById(scriptId).remove();
+      }
 
-    // Append the script to the document body
-    document.body.appendChild(script);
+      // Create a script element
+      const script = document.createElement('script');
+      script.id = scriptId;
+      script.src = '//forms.aweber.com/form/47/954193347.js';
+      script.async = true;
 
-    // Cleanup function to remove the script when the component is unmounted
+      // Append the script to the document body
+      document.body.appendChild(script);
+    };
+
+    // Run addScript on component mount
+    addScript();
+
+    // Clean up function to remove the script when the component is unmounted or on route change
     return () => {
       if (document.getElementById(scriptId)) {
-        document.body.removeChild(script);
+        document.getElementById(scriptId).remove();
       }
     };
-  }, []); // Empty dependency array ensures the effect runs only once
+  }, [router.asPath]); // Effect runs on route changes
 
   return <div className="AW-Form-954193347"></div>;
 };
