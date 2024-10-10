388ed6e93bf21e8ae1ae6c67bb3e47cf4be40127
Naruto92 <m.suhaibmpdgw@gmail.com>
2024-10-10 23:54:13 +0500

    ading backend url's

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -42,7 +42,7 @@ export default function Register() {
         }
 
         try {
-            const response = await axios.post('http://localhost:4848/signup', {
+            const response = await axios.post('https://devsalman.tech/signup', {
                 username,
                 email,
                 password,
